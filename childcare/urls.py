572be02e9a1268e2cdb1e5900d5d62a334ce67a5
Jim-Sloss <j.sloss@btinternet.com>
2025-07-24 12:02:01 +0100

    urls.py updated

--- childcare/urls.py
+++ childcare/urls.py
@@ -51,5 +51,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG: # new
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
